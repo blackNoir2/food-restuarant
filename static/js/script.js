45935301a625e9b47aa47c756b891e84c5753c58
Blacknoir2 <blackNoir2>
2024-03-11 11:05:46 +0000

    Building the second part of the booking which enables a user to select the date, time, and number of guest attending the restaurant.

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,45 @@
-
-
-const closeIcon           =  document.querySelector(".close-icon");
-const openIcon             =  document.querySelector(".open-icon");
-const navigtorLinksElement = document.querySelectorAll(".navigator a");
-const navigatorElement = document.querySelector(".navigator");
+const closeIcon             = document.querySelector(".close-icon");
+const openIcon              = document.querySelector(".open-icon");
+const navigatorLinksElement = document.querySelectorAll(".navigator a");
+const navigatorElement      = document.querySelector(".navigator");
 
 
 closeIcon.addEventListener("click", handleCloseIconClick);
 openIcon.addEventListener("click", handleOpenClickIcon);
 
-function handleCloseIconClick() {
 
-    if (navigtorLinksElement && navigatorElement) {
-      
-        navigtorLinksElement.forEach((linkElement) => {
-            linkElement.classList.add("hidden");
-           
-            closeIcon.classList.add("hidden");
-            openIcon.classList.remove("display-none");
-            
-        })
-         navigatorElement.classList.add("height-hidden");
+
+
+function toggleNavigatorMenu() {
+    if (!navigatorElement || !navigatorLinksElement) {
+        throw new Error("One or more of the selector elements needed could not be found!!!");
     }
-    
+
+    // Check the current state of the navigation menu and toggle accordingly
+    if (navigatorElement.classList.contains("height-hidden")) {
+        handleOpenClickIcon();
+    } else {
+        handleCloseIconClick();
+    }
+}
+
+function handleCloseIconClick() {
+    navigatorLinksElement.forEach((linkElement) => {
+        linkElement.classList.add("hidden");
+    });
+    closeIcon.classList.add("hidden");
+    openIcon.classList.remove("display-none");
+    navigatorElement.classList.add("height-hidden");
 }
 
 function handleOpenClickIcon() {
-    if (navigtorLinksElement && navigatorElement) {
-        navigtorLinksElement.forEach((linkElement) => {
-            linkElement.classList.remove("hidden");
-            
-        }
-        )
-
-        navigatorElement.classList.remove("height-hidden");
-        closeIcon.classList.remove("hidden");
-        openIcon.classList.add("display-none");
-    }
+    navigatorLinksElement.forEach((linkElement) => {
+        linkElement.classList.remove("hidden");
+    });
+    navigatorElement.classList.remove("height-hidden");
+    closeIcon.classList.remove("hidden");
+    openIcon.classList.add("display-none");
 }
+
+
+toggleNavigatorMenu();
